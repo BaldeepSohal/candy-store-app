b415836756d3cf53b9c4f77e3fbfd24899b9f21a
Baldeep Kaur <baldeepkaur@baldeeps-mbp.home>
2024-05-13 11:46:11 -0400

    updated knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -1,15 +1,9 @@
 import type { Knex } from "knex";
 
-// Update with your config settings.
-
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: "mysql",
     connection: {
-      // host: '127.0.0.1',
-      // user: 'root',
-      // password: 'mysql123',
-      // database: 'candystore',
       debug: false,
       port: 3307,
       host: process.env.DB_HOST,
