3cba538d587d73c21b2bedb8b6d367104ac7a3d7
Baldeep Kaur <baldeepkaur@baldeeps-mbp.home>
2024-05-17 11:18:45 -0400

    updated knexfile, added test db credentials

--- knexfile.ts
+++ knexfile.ts
@@ -17,7 +17,20 @@ export const config: { [key: string]: Knex.Config } = {
     }
   },
 
-  staging: {
+  test: {
+    client: "mysql",
+    connection: {
+      debug: false,
+      port: 3307,
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      timezone: "UTC",
+    },
+    migrations: {
+      directory: "./migrations",
+    }
   },
 
   production: {
